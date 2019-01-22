eea5ef92a19a5d5f8508f659c0978b425a86c5f8
Holger Grosse-Plankermann <holgergp@gmail.com>
2019-01-22 15:29:53 +0100

    ID now has same type string ...

--- example-app/src/reducer.js
+++ example-app/src/reducer.js
@@ -5,19 +5,20 @@ export const updateData = (bohne) => ({
     bohne
 });
 
-const INITIAL_STATE = [{id: 1, art: 'Äthiopien', vkp: 30.00, marge: 20.00, ekp: 10.00}];
+const INITIAL_STATE = [{id: '1', art: 'Äthiopien', vkp: 30.00, marge: 20.00, ekp: 10.00}];
 
 const bohnen = (state = INITIAL_STATE, action) => {
-    const bohne  = action.bohne;
+    const bohne = action.bohne;
     switch (action.type) {
         case 'UPDATE_DATA':
             const newState = state.map(s => s.id === bohne.id ? {
-                id: bohne.id,
-                art: bohne.art,
-                vkp: parseFloat(bohne.vkp),
-                marge: parseFloat(bohne.marge),
-                ekp: parseFloat(bohne.ekp)
-            } : s);
+                    id: bohne.id,
+                    art: bohne.art,
+                    vkp: parseFloat(bohne.vkp),
+                    marge: parseFloat(bohne.marge),
+                    ekp: parseFloat(bohne.ekp)
+                } : s
+            );
             return newState;
         default:
             return state;
