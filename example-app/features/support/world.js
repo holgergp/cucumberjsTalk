d7b1d097e18412e40c7d587229cedae5af095796
Holger Grosse-Plankermann <holgergp@gmail.com>
2019-01-23 08:41:46 +0100

    Cleanup

--- example-app/features/support/world.js
+++ example-app/features/support/world.js
@@ -6,7 +6,6 @@ import reducers from '../../src/reducer';
 function BaristaWorld() {
     this.store = createStore(reducers);
     this.wrapper = undefined;
-    this.lastRedirect = undefined;
 }
 
 process.on('unhandledRejection', r => console.error(r));
