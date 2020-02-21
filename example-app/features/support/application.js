eb9d99cc583ff97f70a749d01d1bac55220d1f5c
Holger Grosse-Plankermann <holgergp@gmail.com>
2020-02-21 12:21:05 +0100

    Move helper function

--- example-app/features/support/application.js
+++ example-app/features/support/application.js
@@ -5,3 +5,8 @@ import React from "react";
 export async function oeffneAnwendung(world) {
   world.wrapper = mount(<App store={world.store} />, { attachTo: world.root });
 }
+
+// noinspection JSUnusedGlobalSymbols
+export function logDocument() {
+  console.error(global.jsdom.serialize());
+}
